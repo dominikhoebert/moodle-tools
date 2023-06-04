ceb52c574f0c95e13e51b5f72c86592fb3ea4a36
Dominik <dominik.hoebert@me.com>
2023-06-04 19:46:38 +0200

    added dropdown functionality

--- create_groups.py
+++ create_groups.py
@@ -23,15 +23,18 @@ def create_groups_get():
     if current_user.get_id() is not None:
         students = None
         columns = None
+        course_id = None
         if type(moodle_json := session.get("moodle", None)) is dict:
             current_user.moodle = MoodleSyncTesting.from_json(moodle_json)
             if current_user.moodle.students is not None:
                 students = current_user.moodle.students.to_html(table_id="datatablesSimple")
                 columns = current_user.moodle.students.columns.to_list()
-                logger.debug(f"columns: {columns}")
+                course_id = current_user.moodle.courses[current_user.moodle.course_id]
         return render_template("create_groups.html", username=current_user.get_id(),
                                students=students, courses=current_user.moodle.courses,
-                               columns=columns)
+                               columns=columns, course_id=course_id,
+                               group_column_name=current_user.moodle.group_column_name,
+                               column_name=current_user.moodle.column_name)
     return redirect(url_for("reporting.login"))
 
 
@@ -61,3 +64,36 @@ def file_upload():
             session["moodle"] = current_user.moodle.to_json()
 
         return redirect(url_for('create_groups.create_groups_get'))
+
+
+@create_groups.route("/course/<int:course_id>")
+def course(course_id):
+    if course_id is not None and course_id != 0:
+        if current_user.get_id() is not None:
+            if type(moodle_json := session.get("moodle", None)) is dict:
+                current_user.moodle = MoodleSyncTesting.from_json(moodle_json)
+                current_user.moodle.course_id = course_id
+                session["moodle"] = current_user.moodle.to_json()
+    return redirect(url_for('create_groups.create_groups_get'))
+
+
+@create_groups.route("/column/<column_name>")
+def column(column_name):
+    if column_name is not None and column_name != "":
+        if current_user.get_id() is not None:
+            if type(moodle_json := session.get("moodle", None)) is dict:
+                current_user.moodle = MoodleSyncTesting.from_json(moodle_json)
+                current_user.moodle.column_name = column_name
+                session["moodle"] = current_user.moodle.to_json()
+    return redirect(url_for('create_groups.create_groups_get'))
+
+
+@create_groups.route("/groupname/<group_column_name>")
+def groupname(group_column_name):
+    if group_column_name is not None and group_column_name != "":
+        if current_user.get_id() is not None:
+            if type(moodle_json := session.get("moodle", None)) is dict:
+                current_user.moodle = MoodleSyncTesting.from_json(moodle_json)
+                current_user.moodle.group_column_name = group_column_name
+                session["moodle"] = current_user.moodle.to_json()
+    return redirect(url_for('create_groups.create_groups_get'))
