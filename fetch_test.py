59249cef9cdfbf507913a61b71bcf4ddd8723368
Dominik <dominik.hoebert@me.com>
2023-06-19 22:31:42 +0200

    working on ajax

---
+++ fetch_test.py
@@ -0,0 +1,15 @@
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+
+@app.route("/")
+def index():
+    return render_template("fetch_test.html")
+
+@app.route("/fetch")
+def fetch():
+    return {"message": "Hello, World!"}
+
+
+app.run(debug=True)
