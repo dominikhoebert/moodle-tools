a76c0945697cc11fa0b4c67152474f78fb9ceed9
Dominik <dominik.hoebert@me.com>
2023-07-07 14:01:46 +0200

    gridjs column bg color tests

--- static/js/gridjs_test.js
+++ static/js/gridjs_test.js
@@ -6,13 +6,33 @@ function render_data() {
 
 function parse_response(data) {
     console.log(data)
-    new gridjs.Grid({
+    columns = []
+    for (let [key, value] of Object.entries(data[0])) {
+        //columns.push({name: key, formatter: (cell) => gridjs.html(`<div style="background-color: red">${cell}</div>`)})
+        columns.push({name: key, attributes: {'data-field':  key} })
+    }
+    console.log(columns)
+    const grid = new gridjs.Grid({
+        columns: columns,
         data: data,
         sort: true,
   pagination: true,
   fixedHeader: true,
   resizable: true,
     }).render(document.getElementById("wrapper"));
+
+//    const element = document.querySelector('.gridjs-tr');
+//    element.style.backgroundColor = 'red';
+//    console.log(element)
+
 }
 
 render_data()
+
+function change_style() {
+    element = document.getElementById("gridbg")
+    console.log(element)
+    element.innerHTML = "[data-column-id=name] {background-color: blue !important;}"
+}
+
+change_style()
